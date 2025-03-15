13622e88841b3c8b90a0768161e48ce10a922643
Julianne Duhaze <jduhaze@protonmail.com>
2025-03-15 18:31:20 +0100

    Ajout de l'activité pour ajouter un joueur

--- app/src/main/java/fr/caensup/licsts/smb116_imposteur_app/AddPlayerActivity.java
+++ app/src/main/java/fr/caensup/licsts/smb116_imposteur_app/AddPlayerActivity.java
@@ -1,9 +1,6 @@
 package fr.caensup.licsts.smb116_imposteur_app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AddPlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button btnStartGame = findViewById(R.id.btn_startGame);
-        btnStartGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddPlayerActivity.class);
-                startActivity(intent);
-            }
-        });
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_player);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
